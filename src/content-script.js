205accb249a70aa977ee7bea52608236938f1f6c
manaminakamura <manaminakamura812@gmail.com>
2022-06-15 00:29:23 +0900

    fixed dialog style

--- src/content-script.js
+++ src/content-script.js
@@ -14,25 +14,23 @@ chrome.runtime.onMessage.addListener(
 const showModal = (data) => {
     const modal = document.createElement("dialog");
     modal.setAttribute(
-    "style",`
-    height:450px;
-    border: none;
-    top:150px;
-    border-radius:20px;
-    background-color:white;
-    position: fixed; box-shadow: 0px 12px 48px rgba(29, 5, 64, 0.32);
-    `
+			"style",`
+			max-height: 450px;
+			width: 600px;
+			border: none;
+			top: 150px;
+			border-radius: 10px;
+			background-color: white;
+			position: fixed;
+			`
     );
-    modal.innerHTML = `<div id="popup-content"; style="height:100%">${JSON.stringify(data)}</div>
-    <div style="position:absolute; top:0px; left:5px;">
-    <button style="padding: 8px 12px; font-size: 16px; border: none; border-radius: 20px;">x</button>
-    </div>`;
+    modal.innerHTML = `<div id="popup-content"; style="height: 100%"></div>
+			<div style="position: absolute; top: 5px; right: 40px;">
+			<button style="padding: 8px 12px; font-size: 16px; border: none; border-radius: 20px; position: fixed; cursor: pointer;">×</button>
+			</div>`;
     document.body.appendChild(modal);
     const dialog = document.querySelector("dialog");
     dialog.showModal();
-    // const iframe = document.getElementById("popup-content");
-    // iframe.src = chrome.runtime.getURL("popup.html");
-    // iframe.frameBorder = 0;
     dialog.querySelector("button").addEventListener("click", () => {
         dialog.close();
     });
